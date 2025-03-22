df05ab31f5f42c38b77855ae856c10c3b49fa5f5
bo198214 <bo198214>
2025-03-22 11:32:47 +0100

    removed stuff

--- eatthepie/fetch-game.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-eatthepie ticket-history $1 > lotteries/ticket-history-$1.json
-eatthepie game-info      $1 > lotteries/game-info-$1.json
--- eatthepie/ticket-history.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for n in {13..22}
-do 
-  eatthepie ticket-history $n > lotteries/ticket-history-$n.json
-  eatthepie game-info      $n > lotteries/game-info-$n.json
-done
